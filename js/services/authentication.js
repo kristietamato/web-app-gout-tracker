ae1fe1158a66e9daa094dae91ffc98fb5c619ac9
Kristie Nguyen <kristietamato@gmail.com>
2016-08-04 13:35:10 -0700

    Modify login function; Unknown provider error

--- js/services/authentication.js
+++ js/services/authentication.js
@@ -1,13 +1,13 @@
-myApp.factory('Authentication', ['$rootScope', '$firebase',
-function($rootScope, $firebase) {
+myApp.factory('Authentication', ['$rootScope', '$firebase', 'location'
+function($rootScope, $firebase, $location) {
 
   var auth = firebase.auth();
 
   return {
     login: function (user) {
-      auth.signInWithCredential(user.email,  user.password)
-      .then(function() {
-        $rootScope.message = "Hello " + user.firstname;
+      auth.$authWithPassword(user.email,  user.password)
+      .then(function(regUser) {
+        $location.path('/success');
       }).catch(function (error) {
         $rootScope.message = error.message;
       });
@@ -15,7 +15,15 @@ function($rootScope, $firebase) {
 
     register: function (user) {
       auth.createUserWithEmailAndPassword(user.email, user.password)
-      .then(function(user) {
+      .then(function(regUser) {
+        var regRef = new Firebase('https://gout-tracker.firebaseio.com/' + 'users')
+        .child(regUser.uid).set({
+          date: Firebase.ServerValue.TIMESTAMP,
+          regUser: regUser.uid,
+          firstname: user.firstname,
+          lastname: user.lastname,
+          email: user.email
+        });
         $rootScope.message = "Thanks for registering, " + user.firstname;
       }).catch(function (error) {
         $rootScope.message = error.message;
