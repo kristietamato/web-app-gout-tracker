101d01309b9356c84485ef9a90f0b3cd85308648
Kristie Nguyen <kristietamato@gmail.com>
2016-08-10 11:25:29 -0700

    Create new folders and move files

--- app/app.js
+++ app/app.js
@@ -3,17 +3,17 @@ var myApp = angular.module('myApp', ['ngRoute', 'firebase']);
 myApp.config(['$routeProvider', function($routeProvider) {
   $routeProvider.
     when('/login', {
-      templateUrl: 'partials/login.html',
+      templateUrl: 'authentication/partials/login.html',
       controller: 'AuthController',
       controllerAs: 'authCtrl'
     }).
     when('/register', {
-      templateUrl: 'partials/register.html',
+      templateUrl: 'authentication/partials/register.html',
       controller: 'AuthController',
       controllerAs: 'authCtrl'
     }).
     when('/success', {
-      templateUrl: 'partials/success.html',
+      templateUrl: 'logged-in/partials/success.html',
       controller: 'SuccessController',
       controllerAs: 'authCtrl'
     }).
