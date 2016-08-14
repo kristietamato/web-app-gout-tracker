1f9ad27427f77e838bc37e03cd18df3630acb4f5
Kristie Nguyen <kristietamato@gmail.com>
2016-08-13 18:52:59 -0700

    Add Angular daterangepicker script and module dependency

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-var myApp = angular.module('myApp', ['ngRoute', 'firebase']);
+var myApp = angular.module('myApp', ['ngRoute', 'firebase', 'daterangepicker']);
 
 myApp.config(['$routeProvider', function($routeProvider) {
   $routeProvider.
