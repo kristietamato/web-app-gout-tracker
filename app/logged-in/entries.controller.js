f6f9d38d9b1f461cd4f603df347ab7101ecb83af
Kristie Nguyen <kristietamato@gmail.com>
2016-08-23 19:04:11 -0700

    Add initial Google Chart graph

--- app/logged-in/entries.controller.js
+++ app/logged-in/entries.controller.js
@@ -57,4 +57,41 @@ function($scope, $rootScope, $firebaseArray, $location, EntryService) {
       };
     }
   });
+
+  // Google Charts
+     google.charts.setOnLoadCallback(drawChart);
+
+     function drawChart() {
+       var data = new google.visualization.DataTable();
+       data.addColumn('string', 'Location');
+       data.addColumn('date', 'Start Date of Occurence');
+       data.addColumn('date', 'End Date of Occurence');
+
+       data.addRows([
+         ['Right big toe', new Date(2000, 8, 5), new Date(2001, 1, 5)],
+         ['Left big toe', new Date(2001, 8, 5), new Date(2002, 1, 5)],
+         ['Right ankle', new Date(2002, 8, 5), new Date(2003, 1, 5)],
+         ['Left ankle', new Date(2003, 8, 5), new Date(2004, 1, 5)],
+         ['Right elbow', new Date(2004, 8, 5), new Date(2005, 1, 5)],
+         ['Left elbow', new Date(2005, 8, 5), new Date(2006, 1, 5)],
+         ['Right finger(s)', new Date(2006, 8, 5), new Date(2007, 1, 5)],
+         ['Left finger(s)', new Date(2007, 8, 5), new Date(2008, 1, 5)],
+         ['Other',  new Date(2008, 8, 5), new Date(2009, 1, 5)],
+         ['Right big toe', new Date(2008, 8, 5), new Date(2009, 1, 5)],
+         ['Left big toe', new Date(2010, 8, 5), new Date(2012, 1, 5)],
+         ['Right ankle', new Date(2010, 8, 5), new Date(2013, 1, 5)],
+       ]);
+
+       var options = {
+         height: 450,
+         timeline: {
+           groupByRowLabel: true
+         }
+       };
+
+       var chart = new google.visualization.Timeline(document.getElementById('chart_div'));
+
+       chart.draw(data, options);
+     }
+
 }]);
